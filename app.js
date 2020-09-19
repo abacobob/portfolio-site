07d8bf084319a717e881445726e2fe4077420f69
abacobob <oowdeck@yahoo.co.uk>
2020-09-19 18:34:12 +0200

    procfile changes

--- app.js
+++ app.js
@@ -4,10 +4,4 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-/*
-app.get('/page2', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/page2.html'));
-})
-*/
-
 app.listen(process.env.PORT || port);
