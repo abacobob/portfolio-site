5c74685230583197e3255b104102cb4615a76e14
abacobob <oowdeck@yahoo.co.uk>
2020-09-19 18:29:45 +0200

    added process ports

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const port = 3000;
 
 app.use(express.static("public"));
@@ -11,6 +10,4 @@ app.get('/page2', (req, res) => {
 })
 */
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-});
+app.listen(process.env.PORT || port);
