23da1cf844d5ab6201eb68bf0d94ba5c5ef3a75e
abacobob <oowdeck@yahoo.co.uk>
2020-09-19 19:07:57 +0200

    6

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT);
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+});
