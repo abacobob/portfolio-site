09ca9c3b1a41330ab6d6d7df77d3396c01fb0f3e
abacobob <oowdeck@yahoo.co.uk>
2020-09-19 18:48:00 +0200

    removed local port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT);
